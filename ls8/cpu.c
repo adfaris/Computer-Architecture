96918cfee96c0ac626acd2e0d3e01eb3f70d2093
A.D.Faris <adfaris@gmail.com>
2018-11-26 05:46:30 -0500

    read file

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -18,39 +18,58 @@ void cpu_ram_write(struct cpu *cpu, unsigned char MAR, unsigned char MDR)
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+// void cpu_load(struct cpu *cpu)
+// {
+//   char data[DATA_LEN] = {
+//       // From print8.ls8
+//       // 0b10000010, // LDI R0,8
+//       // 0b00000000,
+//       // 0b00001000,
+//       // 0b01000111, // PRN R0
+//       // 0b00000000,
+//       // 0b00000001 // HLT
+//       0b10000010,
+//       0b00000000,
+//       0b00001000,
+//       0b10000010,
+//       0b00000001,
+//       0b00001001,
+//       0b10100010,
+//       0b00000000,
+//       0b00000001,
+//       0b01000111,
+//       0b00000000,
+//       0b00000001,
+
+//   };
+
+//   int address = 0;
+
+//   for (int i = 0; i < DATA_LEN; i++)
+//   {
+//     cpu->RAM[address++] = data[i];
+//   }
+
+//   // TODO: Replace this with something less hard-coded
+// }
+void cpu_load(struct cpu *cpu, char *argv[])
 {
-  char data[DATA_LEN] = {
-      // From print8.ls8
-      // 0b10000010, // LDI R0,8
-      // 0b00000000,
-      // 0b00001000,
-      // 0b01000111, // PRN R0
-      // 0b00000000,
-      // 0b00000001 // HLT
-      0b10000010,
-      0b00000000,
-      0b00001000,
-      0b10000010,
-      0b00000001,
-      0b00001001,
-      0b10100010,
-      0b00000000,
-      0b00000001,
-      0b01000111,
-      0b00000000,
-      0b00000001,
-
-  };
-
+  char line[100];
   int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++)
+  FILE *fp = fopen(argv[1], "r");
+  if (fp == NULL)
   {
-    cpu->RAM[address++] = data[i];
+    fprintf(stderr, "File pointer is not working");
+    exit(1);
   }
-
-  // TODO: Replace this with something less hard-coded
+  while (fgets(line, sizeof(line), fp))
+  {
+    char *endchar;
+    unsigned long v;
+    v = strtoul(line, &endchar, 2);
+    if (!(*line == *endchar))
+      cpu_ram_write(cpu, address++, v);
+  }
 }
 
 /**
