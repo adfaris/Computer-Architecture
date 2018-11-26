ea698936a09d9c82f426ca11c350d9fcbc24eca0
A.D.Faris <adfaris@gmail.com>
2018-11-26 05:21:05 -0500

    print8 working

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,26 +1,40 @@
 #include "cpu.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 #define DATA_LEN 6
 
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char MAR)
+{
+  return cpu->RAM[MAR];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char MAR, unsigned char MDR)
+{
+  cpu->RAM[MAR] = MDR;
+}
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
 void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
+      // From print8.ls8
+      0b10000010, // LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, // PRN R0
+      0b00000000,
+      0b00000001 // HLT
   };
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  for (int i = 0; i < DATA_LEN; i++)
+  {
+    cpu->RAM[address++] = data[i];
   }
 
   // TODO: Replace this with something less hard-coded
@@ -31,10 +45,11 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  switch (op)
+  {
+  case ALU_MUL:
+    // TODO
+    break;
 
     // TODO: implement more ALU ops
   }
@@ -47,12 +62,31 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
-  while (running) {
+  while (running)
+  {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    unsigned char IR = cpu_ram_read(cpu, cpu->PC);
+    unsigned char operandA = cpu_ram_read(cpu, cpu->PC + 1);
+    unsigned char operandB = cpu_ram_read(cpu, cpu->PC + 2);
     // 2. switch() over it to decide on a course of action.
+    switch (IR)
+    {
+    case LDI:
+      cpu->registers[operandA] = operandB;
+      break;
+
+    case PRN:
+      printf("%d\n", cpu->registers[operandA]);
+      break;
+
+    case HLT:
+      running = 0;
+      break;
+    }
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
+    cpu->PC += (IR >> 6) + 1;
   }
 }
 
@@ -62,6 +96,8 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  memset(cpu->registers, 0, sizeof(cpu->registers));
 
   // TODO: Zero registers and RAM
+  memset(cpu->RAM, 0, sizeof(cpu->RAM));
 }
